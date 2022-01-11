500de2b25a21de5c939dfd78449475612d6e4078
Kaung Phyo Wai <kaungphyowai.mm@gmail.com>
2022-01-11 15:12:05 +0630

    complete

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-const dns = require('dns')
+const dns = require('dns');
+const { arch } = require('os');
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -43,20 +44,26 @@ app.get('/api/shorturl/:id', (req, res) => {
 //Post Url
 app.post('/api/shorturl', (req, res) => {
   let newUrl = req.body.url;
+  console.log(newUrl)
+
   //just get the aa record
   let aRecord = newUrl.replace(/.+?:\/\/(.+)/, "$1");
+  console.log(aRecord)
+
   //validate the new link
-  let acceptedFormat = /[http:// | https://]/.test(newUrl)
+  let acceptedFormat = /http/.test(newUrl)
+  console.log(acceptedFormat)
   if(acceptedFormat){
 
     dns.lookup(aRecord, (err, addr, fam) => {
-        if(addr){
+      // console.log(err)
+        // if(addr){
           let newShortObj = { original_url : newUrl, short_url : list.length + 1}
           list.push(newShortObj)
           res.json(newShortObj)
-        }else{
-          res.json({"error": "Invalid Hostname"});
-        }
+        // }else{
+        //   res.json({"error": "Invalid Hostname"});
+        // }
       }
       )
   }else{
